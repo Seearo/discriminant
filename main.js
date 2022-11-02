2e58be65c21141b310e2cd7a011972e7752a5d75
Seearo <55802117+Seearo@users.noreply.github.com>
2022-11-02 03:45:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 "use strict"
 
 function solveEquation(a, b, c) {
-  
-  let discriminant = Math.pow(b ^ 2 - 4 * a * c);
-  let answer = [];
-  
+
+    let discriminant = Math.pow(b ^ 2 - 4 * a * c);
+    let answer = [];
+
     if (discriminant < 0) {
-         answer = [];
+        answer = [];
     } else if (discriminant = 0) {
-         return answer.push(-b / (2 * a));
+        return answer.push(-b / (2 * a));
     } else if ((discriminant > 0)) {
-         return answer.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
+        return answer.push((-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a));
     }
 
-  
+
     console.log(answer);
 }
 
